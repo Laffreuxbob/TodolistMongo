94b7f8a7d8b40348be1f729cdd6646e80ac7bf70
Laffreuxbob <olivier.bricaud@sfr.fr>
2018-09-26 16:49:42 +0200

    get todos, by id, by name, get infos, post & delete ok + mongo

--- Todo_Backend/todolist.js
+++ Todo_Backend/todolist.js
@@ -5,18 +5,18 @@ const pkg = require('./package.json'); // Pour pouvoir lire les data Json
 const conf = require('./config.js'); // IP et port de notre serveur
 const moment = require('moment'); // Librairie gestion de dates
 
-const mongo = require('./lib/mongo')
+const mongo = require('./lib/mongo') // BDD mongo
 
 const bodyPost = require('body-parser'); // Necessaire a la lecture des data dans le body de la requete (post)
 
-const server = express();
+const server = express(); // Framework gestion serveur nodeJS
 
 server.use(bodyPost.json()); // support json encoded bodies
 server.use(bodyPost.urlencoded({ extended: false })); // support encoded bodies
 
 server.use((req, res, next) => {
-    res.header('Content-Type', 'application/json')
-    next()
+    res.header('Content-Type', 'application/json');
+    next();
 })
 
 // Facon presque propre d'eviter le probleme de header CORS
@@ -37,8 +37,8 @@ server.get('/version', (req, res) => {
         return res.send();
     }
     res.status(200);
-    console.log('version: ' + pkg.version);
-    res.send(JSON.stringify(pkg.version) )
+    console.log('Version: ' + pkg.version);
+    res.send(pkg.version);
     
 })
 
@@ -52,8 +52,8 @@ server.get('/todos', (req, res) => {
             return res.send([]) // return empty array
         }
         res.status(200)
-        console.log("Sending", todos)
-        res.send(todos || {})
+        console.log(todos)
+        res.send(todos || [])
     })
     .catch(err => console.log('An error occured getting mongo todo list', err))
 })
@@ -65,14 +65,14 @@ server.get('/todos/:id', (req, res) => {
     const ID_todoToSearch = req.params.id; /* on recupere l'id' de recherche*/ 
     mongo.db.collection('todos').find({}).toArray()
     .then(todos => {
-        let todoToDisplayFind_ById = todos.find(element => {
-            return(String(element._id) === ID_todoToSearch);
+        let todoToDisplay_ById = todos.find(element => {
+            return((element._id).equals(mongo.ObjectID(ID_todoToSearch)));
         });
         res.status(200);
         console.log('Resultat de la cherche avec l\'id : ' + ID_todoToSearch)
-        console.log(todoToDisplayFind_ById || 'Aucun resultat pour cette recherche');
+        console.log(todoToDisplay_ById || 'Aucun resultat pour cette recherche');
         console.log('------------------------')
-        res.send(todoToDisplayFind_ById); // pas besoin de end() avec send() et stringify transforme ton json en string
+        res.send(todoToDisplay_ById); // pas besoin de end() avec send() et stringify transforme ton json en string
     })    
     .catch(err => console.log('An error occured searching todo with id', err));
 })
@@ -93,18 +93,43 @@ server.get('/todosSearch/:name', (req, res) => {
         res.send(todoToDisplay_ByName); // pas besoin de end() avec send()
     })    
     .catch(err => console.log('An error occured searching todo with name', err));
+});
+// Methode qui renvoie des informations temporelles liees a la tache (delais etc...) avec l'id
+// curl http://127.0.0.1:8080/todosInfos/1
+server.get('/todosInfos/:id', (req, res) => {
+    const ID_todoToSearch = req.params.id; // on recupere les mot cle de recherche
+    mongo.db.collection('todos').find({}).toArray()
+    .then(todos => {
+        let todoGetInfos = todos.find(element => {
+            return((element._id).equals(mongo.ObjectID(ID_todoToSearch)));
+        });
+        return todoGetInfos;
+        res.status(200);
+    })   
+    .then(item => {
+        let dateEnd = moment(item.date,"DD-MM-YYYY" )
+        let dateStart = moment(item.ajout,"DD-MM-YYYY" )
+        
+        let timeAll = dateEnd.diff(dateStart) / (1000 * 60 * 60 * 24);
+        let timeLeft = Math.trunc(dateEnd.diff(moment()) / (1000 * 60 * 60 * 24));
+        
+        console.log('Delai total pour la tache ' + item.name + " : " + timeAll + "jour(s)")
+        console.log('Delai restant pour la tache ' + item.name + " : " + timeLeft + "jour(s)")
+        console.log('------------------------' + '\n')
+        res.send( {"total":timeAll, "restant":timeLeft})
+        res.end();
+    }) 
+    .catch(err => console.log('An error occured searching todo with id', err));
 })
 
 // Methode POST pour ajouter un nouvel element a la liste en cours
 // curl -X POST -H "Content-Type: application/json" -d '{"name":"NouveauProjet", "date":"25-09-2019", "description":"Projet test delai"}' http://localhost:8080/todos/add
 server.post('/todos/add',  (req, res) => {
     const data = req.body    // recuperation des donnees dans le body de la requete
-    
     // attribution des nouvelles key_value  
     let newName = data.name || "default_name"; 
     let newDate = data.date || "11-09-2020";
     let newDescription = data.description || "default_description";
-    
     // creation du nouvel objet tache 
     let newItem = {
         "name":newName,
@@ -112,24 +137,21 @@ server.post('/todos/add',  (req, res) => {
         "ajout": moment().format('DD-MM-YYYY'),
         "description":newDescription,
         "done": false
-    }
-    
+    };
     mongo.db.collection('todos').findOne({name: newItem.name})
     .then(result => {
         if (result) {
-            console.log("Attention, ", newItem.name, "exists!");
-            res.status(403);
-            return res.end();
+            console.log("Warning,", newItem.name, " already exists!");
+            res.status(403).end();
+            return result;
         }
-        
-        return mongo.db.collection('todos').insertOne(newItem)
-    })
-    .then(result => {
-        console.log('Todo successfully added to mongo database.')
-        res.json(newItem)
+        mongo.db.collection('todos').insertOne(newItem);
+        console.log('Todo successfully added to mongo database.');
+        res.status(200);
+        return res.send(newItem);
     })
     .catch(err => {
-        console.log('An error occured inserting todo in mongo.')
+        console.log('An error occured inserting todo in mongo.', err)
     })
 })
 
@@ -137,47 +159,25 @@ server.post('/todos/add',  (req, res) => {
 // Methode DELETE pour supprimer un element de la liste avec une id
 // curl -X DELETE  http://127.0.0.1:8080/delete/2
 server.delete('/delete/:todoToDelete', (req, res) => {
-    const todoToDelete = req.params.todoToDelete
-    todos.forEach( item => {
-        if(parseInt(item.id) === parseInt(todoToDelete)){
-            console.log("Suppresion du projet :" + item.id);
-            let index = todos.indexOf(item);
-            todos.splice(index,1);
+    const ID_todoToDelete = mongo.ObjectID(req.params.todoToDelete);
+    mongo.db.collection('todos').findOne({_id: ID_todoToDelete})
+    .then(result => {
+        console.log(result);
+        if (!result) {
+            console.log("Warning, nothing to delete!");
+            res.status(403).end();
+            return result;
         }
-    }) // utiliser .find()
-    res.status(410); //status de suppression de data du serveur
-    res.end();
+        let itemToDelete = result;
+        mongo.db.collection('todos').deleteOne(itemToDelete);
+        console.log('Todo successfully deleted from mongo database.');
+        res.status(200);
+        return res.end();
+    })
+
 })
 
-// Methode qui renvoie des informations temporelles liees a la tache (delais etc...) avec l'id
-// curl http://127.0.0.1:8080/todosInfos/1
-server.get('/todosInfos/:id', (req, res) => {
-    
-    const todoToSearch = String(req.params.id); // on recupere les mot cle de recherche
-    let todoToGet; // on prepare un objet vide pour recuperer une tache si elle existe 
-    
-    todos.forEach( item => {
-        if(String(item.id) === todoToSearch){
-            todoToGet = item;
-        }
-    })
-    
-    console.log(todoToGet)
-    res.status(200);
-    
-    let dateEnd = moment(todoToGet.date,"DD-MM-YYYY" )
-    let dateStart = moment(todoToGet.ajout,"DD-MM-YYYY" )
-    
-    let timeAll = dateEnd.diff(dateStart) / (1000 * 60 * 60 * 24);
-    let timeLeft = Math.trunc(dateEnd.diff(moment()) / (1000 * 60 * 60 * 24));
-    
-    console.log('Delai total pour la tache ' + todoToSearch + " : " + timeAll + "jour(s)")
-    console.log('Delai restant pour la tache ' + todoToSearch + " : " + timeLeft + "jour(s)")
-    console.log('------------------------' + '\n')
-    
-    res.send( {"total":timeAll, "restant":timeLeft})
-    res.end();
-})
+
 
 
 // Methode d'edition et de mise a jour d'une tache (on utilise le // pour garder la valeur actuelle)
